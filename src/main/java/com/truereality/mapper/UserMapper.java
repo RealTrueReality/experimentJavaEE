ffc5004b014572287dd0e2fc7d42fafbbd971522
truereality <truereality@foxmail.com>
2024-05-26 22:00:00 +0800

    first

--- src/main/java/com/truereality/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.truereality.mapper;
-
-import com.truereality.pojo.User;
-
-import java.util.List;
-
-public interface UserMapper {
-
-}
--- src/main/java/com/truereality/onetomany/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.truereality.onetomany.mapper;
-
-/**
- * @author TrueReality
- * @date 2024/5/25
- * @apiNotes
- */
-public interface UserMapper {
-}
